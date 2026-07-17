f488bf5f131f324bd35dd76858181c09ab588ce4
Ruzilya <ruzila.ramazanova2005@gmail.com>
2026-07-17 10:36:34 +0300

    Update main.py

--- 2 semester/case_task_1/main.py
+++ 2 semester/case_task_1/main.py
@@ -1,21 +1,23 @@
-# Кейс-задача №1. Дан одномерный массив А размерности N. Найти сумму отрицательных элементов, расположенных между максимальным и минимальным. 
+"""
+Кейс-задача №1. Дан одномерный массив А размерности N. 
+Найти сумму отрицательных элементов, расположенных между максимальным и минимальным. 
+"""
 
-N = int(input("Введите кол-во элементов в одномерном массиве: "))
-A = []
-for num in range(N):
-  a = int(input("Введите элемент массива: "))
-  A.append(a)
-max_element = A.index(max(A))
-min_element = A.index(min(A))
+A = [9, 1, -2, 3,-4, 5, -6, 7, -8, -10]
 
-negative_sum=0
-start = min(min_element, max_element)
-finish = max(min_element, max_element)
+min_a = A.index(min(A))
+max_a = A.index(max(A)) 
 
-for element in range(start+1, finish):
-  if A[element] < 0:
-    negative_sum += A[element]
+start = min(min_a, max_a)
+finish = max(min_a, max_a)
 
-print("Массив: ", A)
-print("Максимальный элемент:",max(A), "Минимальный элемент:", min(A))
-print("Сумма негативных элементов:", negative_sum)
+new_A = A[start + 1:finish]
+negative_sum = 0
+for i in new_A:
+    if i < 0:
+        negative_sum += i
+
+print("Массив:", A)
+print("Минимальный элемент:", min(A))
+print("Максимальный элемент:", max(A))
+print("Сумма отрицательных элементов между ними:", negative_sum)
