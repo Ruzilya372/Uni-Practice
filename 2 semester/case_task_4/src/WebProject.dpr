5f9938efad7eb28683ec921faa2e70430e8693a0
Ruzilya <ruzila.ramazanova2005@gmail.com>
2026-07-22 17:25:16 +0300

    Update WebProject.dpr

--- 2 semester/case_task_4/src/WebProject.dpr
+++ 2 semester/case_task_4/src/WebProject.dpr
@@ -1,1 +1,13 @@
+program WebProject;
 
+uses
+  Web.WebBroker,
+  WebModuleUnit1;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TWebModule1, WebModule1);
+  Application.Run;
+end.
